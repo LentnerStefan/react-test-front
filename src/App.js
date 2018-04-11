d0cca4a0e2ba5b5d1141f67737cabb1f05d85164
Christophe Hurpeau <christophe@hurpeau.com>
2018-04-11 17:36:38 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Button } from '@ornikar/kitt';
+import { Paragraph } from '@ornikar/kitt/typography';
+import './App.css';
+
+
+// Series content can be fetched at:
+// http://ornikar.front.test/code/series/series-content/1
+
+// kitt storybook: https://storybook-static-nxppedxrco.now.sh
+
+export default class OrnikarTest extends Component {
+  render() {
+    return (
+      <div className="App">
+
+      </div>
+    );
+  }
+}
