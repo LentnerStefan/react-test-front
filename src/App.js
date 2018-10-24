f8b383b97a54e2f6a3d4a349465a86a45df869a0
Christophe Hurpeau <christophe@hurpeau.com>
2018-10-24 19:32:43 +0200

    chore: update kitt

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React, { Component } from 'react';
-import { Button } from '@ornikar/kitt';
-import { Paragraph } from '@ornikar/kitt/typography';
+import { LargeLoader, Typography } from '@ornikar/kitt';
 import './App.css';
 
 
 // Series content can be fetched at:
-// http://ornikar.front.test/code/series/series-content/1
+// https://static.hurpeau.com/ornikar-serie-1.json
 
-// kitt storybook: https://storybook-static-nxppedxrco.now.sh
+// kitt storybook: https://kitt.ornikar.com
+
+// <img src={`https://onkmedia.blob.core.windows.net/questions/1266/${currentQuestion.question_image_1}.jpg`} />
+
+const Loader = () => <div className="loader"><LargeLoader /></div>;
 
 export default class OrnikarTest extends Component {
   render() {
     return (
-      <div className="App">
-
-      </div>
+      <Typography.div base="body" className="App">
+        <Loader />
+      </Typography.div>
     );
   }
 }
